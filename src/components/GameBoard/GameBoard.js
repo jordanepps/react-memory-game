9b2bdc5abfb1704e13f7f8f29a690e2d72cdb93f
Jordan Epps <jordanepps@gmail.com>
2019-08-30 10:59:56 -0400

    Add animation style to cards when displayed

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
+import { spring } from 'react-flip-toolkit';
 import './GameBoard.css';
 
 import Card from '../Card/Card';
@@ -6,16 +7,46 @@ import { GameContext } from '../../contexts/DataContext';
 
 export default function GameBoard() {
   const [gameData] = useContext(GameContext);
+  const { gameStarted } = gameData;
+  const containerRef = useRef(null);
 
-  function renderGameBoard() {
-    return (
-      <div>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
-    );
+  function renderCards() {
+    //Will render multiple blank cards for animation testing
+    const arr = [];
+    for (let i = 0; i <= 11; i++) {
+      arr.push(<Card key={i} />);
+    }
+    return arr;
   }
-  return gameData.gameStarted ? renderGameBoard() : <div />;
+
+  useEffect(() => {
+    const cards = [...containerRef.current.querySelectorAll('.card')];
+    cards.forEach((el, i) => {
+      spring({
+        config: 'wobbly',
+        values: {
+          translateY: [-15, 0],
+          opacity: [0, 1]
+        },
+        onUpdate: ({ translateY, opacity }) => {
+          el.style.opacity = opacity;
+          el.style.transform = `translateY(${translateY}px)`;
+        },
+        delay: i * 30,
+        onComplete: () => {
+          // add callback logic here if necessary
+        }
+      });
+    });
+  }, [gameStarted]);
+
+  return (
+    <div className="game-board" ref={containerRef}>
+      {/* <Card />
+      <Card />
+      <Card />
+      <Card /> */}
+      {gameStarted ? renderCards() : ''}
+    </div>
+  );
 }
