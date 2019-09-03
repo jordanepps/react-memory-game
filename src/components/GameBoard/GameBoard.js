d88f1dff405ca9dc4e420a6e14d06bdb64ed0440
Jordan Epps <jordanepps@gmail.com>
2019-09-03 14:50:36 -0400

    Add test button to shuffle cards

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect, useRef } from 'react';
-import { spring } from 'react-flip-toolkit';
+import { spring, Flipper, Flipped } from 'react-flip-toolkit';
+import shuffle from 'lodash.shuffle';
 import { GameContext } from '../../contexts/DataContext';
 import './GameBoard.css';
 import Card from '../Card/Card';
@@ -8,37 +9,27 @@ export default function GameBoard() {
   const [gameData] = useContext(GameContext);
   const { gameStarted } = gameData;
   const [allCardsFlipped, setAllCardsFlipped] = useState(true);
+  const [cardData, setCardData] = useState([
+    'a',
+    'a',
+    'b',
+    'b',
+    'c',
+    'c',
+    'd',
+    'd',
+    'e',
+    'e',
+    'f',
+    'f',
+    'g',
+    'g',
+    'h',
+    'h',
+    'i',
+    'i'
+  ]);
   const containerRef = useRef(null);
-
-  function renderCards() {
-    //Will render multiple blank cards for animation testing
-    const cardData = [
-      'a',
-      'a',
-      'b',
-      'b',
-      'c',
-      'c',
-      'd',
-      'd',
-      'e',
-      'e',
-      'f',
-      'f',
-      'g',
-      'g',
-      'h',
-      'h',
-      'i',
-      'i'
-    ];
-    const arr = [];
-    cardData.forEach((card, i) => {
-      arr.push(<Card key={i} icon={card} allCardsFlipped={allCardsFlipped} />);
-    });
-
-    return arr;
-  }
 
   useEffect(() => {
     const cards = [...containerRef.current.querySelectorAll('.card')];
@@ -64,13 +55,27 @@ export default function GameBoard() {
   function flipCards() {
     setTimeout(() => {
       setAllCardsFlipped(false);
-    }, 8500);
+    }, 9000);
+  }
+
+  function shuffleCards() {
+    setCardData(shuffle(cardData));
   }
 
   return (
     <div className="game-board" ref={containerRef}>
-      {gameStarted ? renderCards() : ''}
+      <Flipper flipKey={cardData.join('')}>
+        {gameStarted
+          ? cardData.map((card, i) => (
+              <Flipped key={i} flipId={i}>
+                <Card key={i} icon={card} allCardsFlipped={allCardsFlipped} />
+              </Flipped>
+            ))
+          : ''}
+      </Flipper>
+
       {flipCards()}
+      <button onClick={shuffleCards}>Shuffle</button>
     </div>
   );
 }
