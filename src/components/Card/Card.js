94b40924520de422bac97bfd2ab5846ca73369e3
Jordan Epps <jordanepps@gmail.com>
2019-09-03 14:22:18 -0400

    Remove unecessary code from card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,12 @@
-import React, { useRef, useState, useContext } from 'react';
+import React, { useRef, useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
-import { GameContext } from '../../contexts/DataContext';
+
 import './Card.css';
 
 import CardFront from '../CardFront/CardFront';
 import CardBack from '../CardBack/CardBack';
 
 export default function Card({ icon, allCardsFlipped }) {
-  // const [gameData] = useContext(GameContext);
   const [cardFlipped, setCardFlipped] = useState(false);
   const cardRef = useRef(null);
 
@@ -20,7 +19,6 @@ export default function Card({ icon, allCardsFlipped }) {
     <div className="card" ref={cardRef}>
       <div className="card-container">
         <ReactCardFlip isFlipped={allCardsFlipped || cardFlipped}>
-          {/* <ReactCardFlip isFlipped={allCardsFlipped}> */}
           <CardFront handleFlip={handleFlip} key="front" />
           <CardBack key="back" icon={icon} />
         </ReactCardFlip>
